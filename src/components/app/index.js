f8c176aec3e2f41c15464c76e9c977455c1fab64
Julio Cesar Ody <julio@awesomebydesign.com>
2017-04-02 18:58:51 +1000

    mobx dev tools. Pass search control var to main menu

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,16 +6,19 @@ import style from './index.scss'
 import MainMenu from '../main-menu'
 import Book from '../book'
 import Search from '../search'
+import DevTools from 'mobx-react-devtools'
+import { observer } from 'mobx-react'
 
-const App = ({ store }) => (
+const App = observer(({ store }) => (
   <div className={style.App}>
-    <MainMenu />
+    <MainMenu isSearchVisible={store.ui.isSearchVisible} />
     <Book articles={Array(...store.articles)} />
     <Search
       isVisible={store.ui.isSearchVisible}
-      results={store.searchResults} />
+      results={Array(...store.searchResults)} />
+    {process.env.NODE_ENV === 'development' && <DevTools />}
   </div>
-)
+))
 
 App.propTypes = {
   store: PropTypes.object.isRequired
